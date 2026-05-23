f0a2196b5c1817ce6e85ff1e83c93ad12284dea5
Kyle Oliver <oliverke@mail.uc.edu>
2026-05-23 11:30:24 -0400

    feat: add basic application baseline

---
+++ src/ohio_unemployment/log.py
@@ -0,0 +1,16 @@
+"""Module containing logic for logging used throughout the ohio_unemployment package."""
+from pathlib import Path
+
+from loguru import logger
+
+from ohio_unemployment.constants import APP_START_TIME
+from ohio_unemployment.constants import USER_LOG_FOLDER
+from ohio_unemployment.constants import _FILE_SAFE_DATETIME_FORMAT
+
+
+_FILE_SAFE_DATETIME_SLUG: str = APP_START_TIME.strftime(_FILE_SAFE_DATETIME_FORMAT)
+
+LOG_PATH: Path = USER_LOG_FOLDER / f"log_{_FILE_SAFE_DATETIME_SLUG}.log"
+
+
+logger.add(LOG_PATH, serialize=True)
