ef0d0fafbc0470b550104d393024b41557224024
Kyle Oliver <oliverke@mail.uc.edu>
2026-05-25 01:46:41 -0400

    bump: version 0.0.0 → 0.1.0

---
+++ src/ohio_unemployment/document.py
@@ -0,0 +1,13 @@
+"""Module containing logic and data structures for interacting with Ohio Unemployment documents."""
+from collections import namedtuple
+from pathlib import Path
+from typing import Any
+from typing import NamedTuple
+from typing import Optional
+
+import pydicom
+from PIL import Image
+
+
+if __name__ == "__main__":
+    pass
